1181d58ed19f7f42d94c33c8838671a83b884ad4
vtec11001 <smn73822@uga.edu>
2017-04-13 18:11:16 -0400

    Add files via upload

--- MiningRunner.java
+++ MiningRunner.java
@@ -4,11 +4,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import com.mysql.jdbc.ResultSetImpl;
@@ -16,15 +18,31 @@ import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationExceptio
 import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
 
 import twitter4j.GeoLocation;
+import twitter4j.Location;
 import twitter4j.Paging;
 import twitter4j.Query;
 import twitter4j.QueryResult;
+import twitter4j.ResponseList;
 import twitter4j.Status;
+import twitter4j.Trends;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.User;
 import twitter4j.conf.ConfigurationBuilder;
+
+
+// // This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 public class MiningRunner {
 	
@@ -32,7 +50,7 @@ public class MiningRunner {
 	// Driver name
 	private final static String driver = "com.mysql.jdbc.Driver";
 	// Databasename :
-	private final static String url = "jdbc:mysql://127.0.0.1:3306/twitter_info";
+	private final static String url = "jdbc:mysql://127.0.0.1:3306/tweet_storage";
 	// Mysql username
 	private final static String user = "root";
 	// Mysql password
@@ -41,48 +59,44 @@ public class MiningRunner {
 	
 	
 
-	public static void main(String[] args) throws TwitterException, SQLException 
+	public static void main(String[] args) throws TwitterException, SQLException, InterruptedException 
 	{
-		// TODO Auto-generated method stub
 
 		
-		ArrayList<String>testing = new ArrayList<String>(); 
-		ArrayList<String>testing2 = new ArrayList<String>(); 
-		
-		testing = (ArrayList<String>) getTimeLine(); 
-		testing2 = (ArrayList<String>) searchtweets(); 
-				
-		/*
-		for (int i = 0; i<testing.size(); i++)
-		{
-			System.out.println(testing.get(i) + " " + i); 
-		}
-		*/
-		
-/*
-			testing2 = (ArrayList<String>) searchtweets();
-			
-			for (int i = 0; i<testing2.size(); i++)
-			{
-				System.out.println(testing2.get(i)); 
-			}
-		
-		*/
-		/*
-		ArrayList<Status> testData = searchLocation(); 
-		
-		for (Status s : testData)
-		{
-			System.out.println((s.getUser()).getId());
-		}
-		
-		//insertData(); 
-		 * */
+		        HttpClient httpclient = HttpClients.createDefault();
+
+		        try
+		        {
+		            URIBuilder builder = new URIBuilder("https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment");
+
+
+		            URI uri = builder.build();
+		            HttpPost request = new HttpPost(uri);
+		            request.setHeader("Content-Type", "application/json");
+		            request.setHeader("Ocp-Apim-Subscription-Key", "{subscription key}");
+
+
+		            // Request body
+		            StringEntity reqEntity = new StringEntity("{body}");
+		            request.setEntity(reqEntity);
+
+		            HttpResponse response = httpclient.execute(request);
+		            HttpEntity entity = response.getEntity();
+
+		            if (entity != null) 
+		            {
+		                System.out.println(EntityUtils.toString(entity));
+		            }
+		        }
+		        catch (Exception e)
+		        {
+		            System.out.println(e.getMessage());
+		        }
+		    
 		
 		
-		//gatherUsers(); 
-		addInfo(); 
 		
+		getTrending(); 
 		
 		
 	}
@@ -345,6 +359,7 @@ public class MiningRunner {
 				{
 					User tempUser = twitter.showUser(tempLong); 
 					String tempUsername = tempUser.getName(); 
+					//double lat = (tempUser
 					
 					query = "update twitter_users set username = \"" + tempUsername    + "\" where user_id = " + tempLong; 
 					
@@ -365,8 +380,101 @@ public class MiningRunner {
 			
 		}
 	}
+
 	
+	public static void addData() throws SQLException, TwitterException
+	{
+		Twitter twitter = getTwitterinstance(); 
+		
+		initDatabase(); 
+		Statement statement = null; 
+		statement = conn.createStatement(); 
+		String query1 = null;
+		String query2 = null; 
+		
+		ArrayList<Status> listStatus = searchLocation(); 
+		
+		for (Status s : listStatus)
+		{
+			User tempuser = s.getUser(); 
+			
+			String username = tempuser.getName(); 
+			long user_id = tempuser.getId(); 
+			
+			String tweetinfo = s.getText(); 
+			
+			
+			query1 = "INSERT INTO USER(USER_ID, USERNAME) VALUES(?, ?)"; 
+			query2 = "INSERT INTO tweet_data (tweet, tweet_id) VALUES(?, ?)"; 
 	
+			try
+			{
+				statement = conn.prepareStatement(query1); 
+				((PreparedStatement) statement).setLong(1, user_id); 
+				((PreparedStatement) statement).setString(2, username); 
+				((PreparedStatement)statement).executeUpdate(); 
+				 
+				
+				
+				
+				statement = conn.prepareStatement(query2); 
+				((PreparedStatement) statement).setString(1, tweetinfo); 
+				((PreparedStatement) statement).setLong(2, user_id); 
+				((PreparedStatement)statement).executeUpdate(); 
+			
+			}
+			
+			catch (MySQLIntegrityConstraintViolationException m)
+			{
+				
+				statement = conn.prepareStatement(query2); 
+				((PreparedStatement) statement).setString(1, tweetinfo); 
+				((PreparedStatement) statement).setLong(2, user_id); 
+				((PreparedStatement)statement).executeUpdate();
+				
+				continue; 
+			}
+			
+		}
+		
+		
+		
+
+	}
+
+	
+	public static void getTrending() throws SQLException, TwitterException
+	{
+		
+		// TODO Auto-generated method stub
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("GUJPXGn05nx82B7d3GcWpnJxd")
+		  .setOAuthConsumerSecret("PFe1cwu5uG8wpzoWO0cxL1IWxSpp2ZsgFNxEdlPCd7ZqnOtyxo")
+		  .setOAuthAccessToken("850074770859339776-RwADwOwrXNL7q7LuJDOnLH5o7OvXVRx")
+		  .setOAuthAccessTokenSecret("Jav9EdPvMkqZYz1z5KU3hCtcNOr5gGzCHE7BGvzFhSadb");
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		
+		//TwitterFactory tf = new TwitterFactory(getTwitterinstance());
+		Twitter twitter = tf.getInstance();  
+		
+		initDatabase(); 
+		Statement statement = null; 
+		statement = conn.createStatement(); 
+		
+		ResponseList<Location>CurrentTrends;  
+		CurrentTrends = twitter.getAvailableTrends(); 
+		
+		Trends trendData = twitter.getPlaceTrends(2357024); 
+		
+		for (int i = 0; i<trendData.getTrends().length; i++)
+		{
+			System.out.println(trendData.getTrends()[i].getName());
+		}
+		
+		
+		
+	}
 	
 
 }
